73ed92bc3073c3094e6eeeb8cee2a95069a1f914
Md Mojahid <mojahidmd92@gmail.com>
2026-04-04 16:46:17 +0530

    chore: initial project setup with React, TypeScript, Vite, Tailwind, Prettier, ESLint, and Husky

---
+++ lint-staged.config.js
@@ -0,0 +1,4 @@
+export default {
+  "*.{js,jsx,ts,tsx}": ["eslint", "prettier --write"],
+  "*.{css,scss,md,html}": ["prettier --write"],
+};
---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <h1 className="text-xl">Hello, Developers!</h1>;
+}
+
+export default App;
